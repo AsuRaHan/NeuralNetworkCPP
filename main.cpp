500f9c2b75f44a898d263de3e0b2ce6cfe0b9ddd
Ivan Kolotilkin <logic@xaker.ru>
2023-03-30 00:25:19 +0400

    alpha version

--- main.cpp
+++ main.cpp
@@ -1,177 +1,329 @@
-﻿#include <fstream>
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <random>
-#include <chrono>
-#include <numeric>
 #include <cmath>
-#include <cassert>
 #include <cstdlib>
 #include <ctime>
+
+#include <fstream>
+#include <iostream>
 #include <sstream>
-#include <memory>
-#include <execution>
+#include <random>
 
-#include "NeuralNetwork.h"
-
-
-
-
-class IrisClassifier {
-public:
-    IrisClassifier(const std::string& filename, int num_hidden_neurons)
-        : inputs(), outputs(), training_indices(), num_output_neurons(3), learning_rate(0.1), net({ 4, num_hidden_neurons, num_output_neurons },0.1)
-    {
-        // Load the data from a file
-        load_data(filename, inputs, outputs);
-
-        // Initialize the training indices vector
-        /*for (int i = 0; i < inputs.size(); ++i) {
-            training_indices.push_back(i);
-        }*/
-
-        // Seed the random number generator
-        std::srand(std::time(nullptr));
-
-        // Shuffle the training indices vector
-        std::shuffle(training_indices.begin(), training_indices.end(), std::default_random_engine(std::rand()));
+void load_dataset(std::string filename, std::vector<std::vector<double>>& inputs, std::vector<std::vector<double>>& outputs) {
+    std::ifstream file(filename);
+    if (!file) {
+        throw std::runtime_error("Failed to open file: " + filename);
     }
-    void shuffle_indices(std::vector<int>& indices) {
-        // Seed the random number generator
-        std::srand(std::time(nullptr));
 
-        // Shuffle the indices vector
-        std::shuffle(indices.begin(), indices.end(), std::default_random_engine(std::rand()));
-    }
-    void load_data(std::string filename, std::vector<std::vector<double>>& inputs, std::vector<std::vector<double>>& outputs) {
-        std::ifstream file(filename);
-        if (!file) {
-            throw std::runtime_error("Failed to open file: " + filename);
+    std::string line;
+    int wIter = 0;
+    while (std::getline(file, line)) {
+        if (line.empty()) {
+            continue;
         }
+        std::stringstream ss(line);
+        std::string token;
+        std::vector<double> input_row;
+        std::vector<double> output_row;
 
-        std::string line;
-        int wIter = 0;
-        while (std::getline(file, line)) {
-            if (line.empty()) {
-                continue;
-            }
-            std::stringstream ss(line);
-            std::string token;
-            std::vector<double> input_row;
-            std::vector<double> output_row;
-
-            // Read the input values
-            for (int i = 0; i < 4; i++) {
-                if (std::getline(ss, token, ',')) {
-                    input_row.push_back(std::stod(token));
-                }
-                else {
-                    throw std::runtime_error("Invalid data format in file: " + filename);
-                }
-            }
-
-            // Read the output values
-            if (std::getline(ss, token)) {
-                if (token == "Iris-setosa") {
-                    output_row = { 1.0, 0.0, 0.0 };
-                }
-                else if (token == "Iris-versicolor") {
-                    output_row = { 0.0, 1.0, 0.0 };
-                }
-                else if (token == "Iris-virginica") {
-                    output_row = { 0.0, 0.0, 1.0 };
-                }
-                else {
-                    throw std::runtime_error("Invalid data format in file: " + filename);
-                }
+        // Read the input values
+        for (int i = 0; i < 4; i++) {
+            if (std::getline(ss, token, ',')) {
+                input_row.push_back(std::stod(token));
             }
             else {
                 throw std::runtime_error("Invalid data format in file: " + filename);
             }
-
-            inputs.push_back(input_row);
-            outputs.push_back(output_row);
-            training_indices.push_back(wIter);
-            wIter++;
         }
 
-        file.close();
-    }
-
-    void train(int num_epochs) {
-        shuffle_indices(training_indices);
-
-        // Shuffle the data
-        //std::shuffle(training_indices.begin(), training_indices.end(),std::default_random_engine(std::chrono::system_clock::now().time_since_epoch().count()));
-        
-        // Train the neural network
-        for (int epoch = 1; epoch <= num_epochs; ++epoch) {
-            double total_error = 0.0;
-            for (const auto& i : training_indices) {
-                std::vector<double> predicted = net.feedforward(inputs[i]);
-                std::vector<double> error(outputs[i].size());
-                std::transform(outputs[i].begin(), outputs[i].end(), predicted.begin(), error.begin(), std::minus<double>());
-                net.backpropagate(inputs[i], outputs[i]);
-                total_error += std::inner_product(error.begin(), error.end(), error.begin(), 0.0);
+        // Read the output values
+        if (std::getline(ss, token)) {
+            if (token == "Iris-setosa") {
+                output_row = { 1.0, 0.0, 0.0 };
             }
-            std::cout << "Epoch " << epoch << " error: " << total_error << std::endl;
+            else if (token == "Iris-versicolor") {
+                output_row = { 0.0, 1.0, 0.0 };
+            }
+            else if (token == "Iris-virginica") {
+                output_row = { 0.0, 0.0, 1.0 };
+            }
+            else {
+                throw std::runtime_error("Invalid data format in file: " + filename);
+            }
+        }
+        else {
+            throw std::runtime_error("Invalid data format in file: " + filename);
         }
 
-        net.save_model("models.txt");
+        inputs.push_back(input_row);
+        outputs.push_back(output_row);
+        wIter++;
     }
 
-    std::string classify(double sepal_length, double sepal_width, double petal_length, double petal_width) {
-        std::vector<double> input = { sepal_length, sepal_width, petal_length, petal_width };
-        std::vector<double> output = net.feedforward(input);
-        int class_index = std::distance(output.begin(), std::max_element(output.begin(), output.end()));
-        switch (class_index) {
-        case 0: return "Iris Setosa";
-        case 1: return "Iris Versicolour";
-        case 2: return "Iris Virginica";
-        default: throw std::runtime_error("Unknown class index");
-        }
-    }
+    file.close();
+}
 
+bool fileExists(const std::string& fileName) {
+    std::ifstream infile(fileName.c_str());
+    return infile.good();
+}
+
+// Функция перемешивания данных
+void shuffle_data(std::vector<std::vector<double>>& inputs, std::vector<std::vector<double>>& targets) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(inputs.begin(), inputs.end(), gen);
+    std::shuffle(targets.begin(), targets.end(), gen);
+}
+
+class NeuralNetwork {
 private:
-    std::vector<std::vector<double>> inputs;
-    std::vector<std::vector<double>> outputs;
-    std::vector<int> training_indices;
-    int num_output_neurons;
-    double learning_rate;
-    NeuralNetwork net;
+    int inputNodes;
+    int hiddenNodes;
+    int outputNodes;
+    double learningRate;
+
+    std::vector<std::vector<double>> weightsInputHidden;
+    std::vector<std::vector<double>> weightsHiddenOutput;
+
+public:
+    NeuralNetwork(int input, int hidden, int output, double rate = 0.1) {
+        inputNodes = input;
+        hiddenNodes = hidden;
+        outputNodes = output;
+        learningRate = rate;
+
+        weightsInputHidden.resize(inputNodes, std::vector<double>(hiddenNodes));
+        weightsHiddenOutput.resize(hiddenNodes, std::vector<double>(outputNodes));
+        randomizeWeights();
+    }
+
+    void train(std::vector<double> inputs, std::vector<double> targets) {
+        std::vector<double> hiddenOutputs(hiddenNodes);
+        std::vector<double> finalOutputs(outputNodes);
+
+        // Forward propagation
+        for (int i = 0; i < hiddenNodes; i++) {
+            double sum = 0.0;
+            for (int j = 0; j < inputNodes; j++) {
+                sum += inputs[j] * weightsInputHidden[j][i];
+            }
+            hiddenOutputs[i] = sigmoid(sum);
+        }
+
+        for (int i = 0; i < outputNodes; i++) {
+            double sum = 0.0;
+            for (int j = 0; j < hiddenNodes; j++) {
+                sum += hiddenOutputs[j] * weightsHiddenOutput[j][i];
+            }
+            finalOutputs[i] = sigmoid(sum);
+        }
+
+        // Backpropagation
+        std::vector<double> outputErrors(outputNodes);
+        for (int i = 0; i < outputNodes; i++) {
+            outputErrors[i] = (targets[i] - finalOutputs[i]) * sigmoidDerivative(finalOutputs[i]);
+        }
+
+        std::vector<double> hiddenErrors(hiddenNodes);
+        for (int i = 0; i < hiddenNodes; i++) {
+            double error = 0.0;
+            for (int j = 0; j < outputNodes; j++) {
+                error += outputErrors[j] * weightsHiddenOutput[i][j];
+            }
+            hiddenErrors[i] = error * sigmoidDerivative(hiddenOutputs[i]);
+        }
+
+        // Update weights
+        for (int i = 0; i < hiddenNodes; i++) {
+            for (int j = 0; j < inputNodes; j++) {
+                weightsInputHidden[j][i] += learningRate * hiddenErrors[i] * inputs[j];
+            }
+        }
+
+        for (int i = 0; i < outputNodes; i++) {
+            for (int j = 0; j < hiddenNodes; j++) {
+                weightsHiddenOutput[j][i] += learningRate * outputErrors[i] * hiddenOutputs[j];
+            }
+        }
+    }
+
+    std::vector<double> predict(std::vector<double> inputs) {
+        std::vector<double> hiddenOutputs(hiddenNodes);
+        std::vector<double> finalOutputs(outputNodes);
+
+        for (int i = 0; i < hiddenNodes; i++) {
+            double sum = 0.0;
+            for (int j = 0; j < inputNodes; j++) {
+                sum += inputs[j] * weightsInputHidden[j][i];
+            }
+            hiddenOutputs[i] = sigmoid(sum);
+        }
+
+        for (int i = 0; i < outputNodes; i++) {
+            double sum = 0.0;
+            for (int j = 0; j < hiddenNodes; j++) {
+                sum += hiddenOutputs[j] * weightsHiddenOutput[j][i];
+            }
+            finalOutputs[i] = sigmoid(sum);
+        }
+
+        return finalOutputs;
+    }
+
+    void saveModelRaw(std::string fileName) {
+        FILE* file;
+        if (fopen_s(&file, fileName.c_str(), "wb") != 0) {
+            std::cerr << "Ошибка открытия файла для записи: " << fileName << std::endl;
+            return;
+        }
+
+        fwrite(&inputNodes, sizeof(int), 1, file);
+        fwrite(&hiddenNodes, sizeof(int), 1, file);
+        fwrite(&outputNodes, sizeof(int), 1, file);
+
+        for (int i = 0; i < inputNodes; i++) {
+            fwrite(&weightsInputHidden[i][0], sizeof(double), hiddenNodes, file);
+        }
+
+        for (int i = 0; i < hiddenNodes; i++) {
+            fwrite(&weightsHiddenOutput[i][0], sizeof(double), outputNodes, file);
+        }
+
+        fclose(file);
+    }
+    // Метод для загрузки модели из файла
+    void loadModelRaw(std::string fileName) {
+        FILE* file;
+        if (fopen_s(&file, fileName.c_str(), "rb") != 0) {
+            std::cerr << "Ошибка открытия файла для чтения: " << fileName << std::endl;
+            return;
+        }
+
+        fread(&inputNodes, sizeof(int), 1, file);
+        fread(&hiddenNodes, sizeof(int), 1, file);
+        fread(&outputNodes, sizeof(int), 1, file);
+
+        weightsInputHidden.resize(inputNodes, std::vector<double>(hiddenNodes, 0.0));
+        for (int i = 0; i < inputNodes; i++) {
+            fread(&weightsInputHidden[i][0], sizeof(double), hiddenNodes, file);
+        }
+
+        weightsHiddenOutput.resize(hiddenNodes, std::vector<double>(outputNodes, 0.0));
+        for (int i = 0; i < hiddenNodes; i++) {
+            fread(&weightsHiddenOutput[i][0], sizeof(double), outputNodes, file);
+        }
+
+        fclose(file);
+    }
+private:
+
+    void randomizeWeights() {
+        for (int i = 0; i < inputNodes; i++) {
+            for (int j = 0; j < hiddenNodes; j++) {
+                weightsInputHidden[i][j] = randomWeight();
+            }
+        }
+
+        for (int i = 0; i < hiddenNodes; i++) {
+            for (int j = 0; j < outputNodes; j++) {
+                weightsHiddenOutput[i][j] = randomWeight();
+            }
+        }
+    }
+
+    double randomWeight() {
+        return (double)rand() / RAND_MAX - 0.5;
+    }
+
+    double sigmoid(double x) {
+        return 1.0 / (1.0 + exp(-x));
+    }
+
+    double sigmoidDerivative(double x) {
+        return x * (1 - x);
+    }
+
+    float relu(float x) {
+        return std::max(0.0f, x);
+    }
+
+    float relu_prime(float x) {
+        return (x >= 0) ? 1.0f : 0.0f;
+    }
 };
 
 
 int main() {
-    try {
-        // Create an instance of the IrisClassifier
-        IrisClassifier classifier("iris.data", 5);
+    setlocale(LC_CTYPE, "Russian");
 
-        // Train the classifier
-        classifier.train(100);
+    std::vector<std::vector<double>> inputs;
+    std::vector<std::vector<double>> outputs;
 
-        // Test the classifier
-        std::cout << "Classifying new iris:" << std::endl;
-        std::cout << "Sepal length (cm): ";
-        double sepal_length;
-        std::cin >> sepal_length;
-        std::cout << "Sepal width (cm): ";
-        double sepal_width;
-        std::cin >> sepal_width;
-        std::cout << "Petal length (cm): ";
-        double petal_length;
-        std::cin >> petal_length;
-        std::cout << "Petal width (cm): ";
-        double petal_width;
-        std::cin >> petal_width;
-        std::string result = classifier.classify(sepal_length, sepal_width, petal_length, petal_width);
-        std::cout << "The iris is a " << result << "." << std::endl;
+    std::string fileName = "model.data";
+
+    // Create neural network
+    NeuralNetwork nn(4, 8, 3, 0.1);
+
+    if (!fileExists(fileName)) {
+        std::cout << "Файл " << fileName << " не найден." << std::endl;
+        load_dataset("iris.data", inputs, outputs);
+        //shuffle_data(inputs, outputs);
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < inputs.size(); j++) {
+                nn.train(inputs[j], outputs[j]);
+            }
+        }
+
+
+        // Test neural network
+        int correct = 0;
+        for (int i = 0; i < inputs.size(); i++) {
+            std::vector<double> output = nn.predict(inputs[i]);
+            int prediction = max_element(output.begin(), output.end()) - output.begin();
+            std::cout << "The iris is a \033[1;31m" << prediction << "\033[0m = {" << outputs[i][0] << "," << outputs[i][1] << "," << outputs[i][2] << "} i=" << i << std::endl;
+            std::cout << "The output = {" << output[0] << "," << output[1] << "," << output[2] << "}" << std::endl << std::endl;
+            int target = max_element(inputs[i].begin(), inputs[i].end()) - inputs[i].begin();
+            if (prediction == target) {
+                correct++;
+            }
+        }
+
+        std::cout << "Accuracy: " << (double)correct / inputs.size() << std::endl;
+
+
+        nn.saveModelRaw(fileName);
+
+
     }
-    catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
+    else {
+        std::cout << "Файл " << fileName << " существует!" << std::endl;
+        nn.loadModelRaw(fileName);
     }
+
+
+    // Test the classifier
+    std::cout << "Classifying new iris:" << std::endl;
+    std::cout << "Sepal length (cm): ";
+    double sepal_length;
+    std::cin >> sepal_length;
+    std::cout << "Sepal width (cm): ";
+    double sepal_width;
+    std::cin >> sepal_width;
+    std::cout << "Petal length (cm): ";
+    double petal_length;
+    std::cin >> petal_length;
+    std::cout << "Petal width (cm): ";
+    double petal_width;
+    std::cin >> petal_width;
+
+    std::vector<double> input = { sepal_length, sepal_width, petal_length, petal_width };
+    std::vector<double> output = nn.predict(input);
+    int class_index = std::distance(output.begin(), std::max_element(output.begin(), output.end()));
+
+    std::cout << "The iris is a " << class_index << "." << std::endl;
+
     return 0;
 }
